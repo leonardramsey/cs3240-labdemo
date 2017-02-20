aebaf321bd38af9bb1fa43534e17cf20f6f68071
Leonard Ramsey <Leonard Ramsey>
2017-02-20 17:09:18 -0500

    Second commit

--- hello.py
+++ hello.py
@@ -1,1 +1,15 @@
-print("hello")
+__author__="lr3hj"
+# Leonard Ramsey
+# 2/20/17
+# CS3240 Lab 5
+# hello.py
+
+
+
+# print("hello")
+
+def greetings(msg):
+    print(str(msg))
+
+if __name__ == "__main__":
+    greetings("hello")
